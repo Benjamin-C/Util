9377cccd36794822aa5d9cf479717d013255b5c0
Benjamin-C <bencrall@gmail.com>
2020-03-30 21:04:06 -0700

    Added pausing and an option to trace

--- src/util/Logger.java
+++ src/util/Logger.java
@@ -16,37 +16,74 @@ public class Logger {
 	private PrintStream defaultSystemOut;
 	private PrintStream defaultSystemErr;
 	
+	private boolean paused;
+	private boolean active;
+	
+	private boolean traced;
+	private String nameEnd;
+	
 	public Logger() {
+		this(true, "");
+	}
+	public Logger(boolean traced, String nameEnd) {
 		defaultSystemOut = System.out;
 		defaultSystemErr = System.err;
+		this.traced = traced;
+		this.nameEnd = nameEnd;
 	}
 	
 	public void start() {
-		DateFormat logPath = new SimpleDateFormat("yyyy/MM/dd");
- 		DateFormat logName = new SimpleDateFormat("yyyyMMddHHmmss");
- 		Date cal = new Date();
- 		String logPathStr = "logs/" + logPath.format(cal);
- 		String logNameStr = logName.format(cal) + ".txt";
- 		System.out.println("logname =" + logPathStr  + "/" + logNameStr);
- 		File f = new File(logPathStr);
- 		f.mkdirs();
- 		try {
-			logPrintStream = new PrintStream(new PrintStream(logPathStr + "/" + logNameStr), true);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if(!active) {
+			active = true;
+			DateFormat logPath = new SimpleDateFormat("yyyy/MM/dd");
+	 		DateFormat logName = new SimpleDateFormat("yyyyMMddHHmmss");
+	 		Date cal = new Date();
+	 		String logPathStr = "logs/" + logPath.format(cal);
+	 		String logNameStr = logName.format(cal) + nameEnd + ".txt";
+	 		System.out.println("logname =" + logPathStr  + "/" + logNameStr);
+	 		File f = new File(logPathStr);
+	 		f.mkdirs();
+	 		try {
+				logPrintStream = new PrintStream(new PrintStream(logPathStr + "/" + logNameStr), true);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	 		outputPrintStream = new PrintStreamDuplicator(logPrintStream, System.out);
+	 		if(traced) {
+	 			tracedPrintStream = new TracedPrintStream(outputPrintStream, true);
+	 		} else {
+	 			tracedPrintStream = outputPrintStream;
+	 		}
+	 		System.setOut(tracedPrintStream);
+	 		System.setErr(outputPrintStream);
+	 		logPrintStream.flush();
 		}
- 		outputPrintStream = new PrintStreamDuplicator(logPrintStream, System.out);
- 		tracedPrintStream = new TracedPrintStream(outputPrintStream, true);
- 		System.setOut(tracedPrintStream);
- 		System.setErr(outputPrintStream);
- 		System.out.println("Jeopardy");
- 		logPrintStream.flush();
+	}
+	
+	public void pause() {
+		if(active && !paused) {
+			paused = true;
+			System.setOut(defaultSystemOut);
+	 		System.setErr(defaultSystemErr);
+		}
+	}
+	
+	public void resume() {
+		if(paused) {
+			System.setOut(tracedPrintStream);
+	 		System.setErr(outputPrintStream);
+		}
 	}
 	
 	public void stop() {
-		logPrintStream.close();
-		System.setOut(defaultSystemOut);
- 		System.setErr(defaultSystemErr);
+		if(active) {
+			active = false;
+			logPrintStream.close();
+			if(!paused) {
+				System.setOut(defaultSystemOut);
+		 		System.setErr(defaultSystemErr);
+			}
+		}
 	}
 }
